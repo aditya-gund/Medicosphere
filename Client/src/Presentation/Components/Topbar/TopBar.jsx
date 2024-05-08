b53bff9301a0d21934c1513ba9de59e82ef1b0a3
loronto <46075089+loronto@users.noreply.github.com>
2024-05-08 16:54:18 +0530

    changed images background

--- Client/src/Presentation/Components/Topbar/TopBar.jsx
+++ Client/src/Presentation/Components/Topbar/TopBar.jsx
@@ -3,16 +3,16 @@ import "./TopBar.css";
 function TopBar(){
     return <div className="TopBar">
         <div className="searchBar">
-            <img src="search.svg" alt="search"/>
+            <img src="./search.svg" alt="search"/>
             <input type="text" placeholder="Enter event id to search" />
         </div>
         <div className="TopBarOptions">
             <div>Hello username</div>
-            <img src="user.svg" alt="user" style={{borderRadius: "50%", backgroundColor: "#dddddd", padding: "2px"}} />
-            <img src="notifications.svg" alt="notifications" />
+            <img src="./user.svg" alt="user" style={{borderRadius: "50%", backgroundColor: "#dddddd", padding: "2px"}} />
+            <img src="./notifications.svg" alt="notifications" />
             <button>
                 <div>Logout</div>
-                <img src="logOut.svg" alt="logout" />
+                <img src="./logOut.svg" alt="logout" />
             </button>
         </div>
     </div>
